90256b59e530643eab2d3d6a80946b4a5527ce00
蔡府宏 <cfh12580@gmail.com>
2021-10-03 20:23:28 +0900

    分支

---
+++ fenzhi.c
@@ -0,0 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//int main()
+//{
+//	int age = 0;
+//	scanf("%d", &age);
+//
+//	if (age<18)
+//	{
+//		printf("shaonian\n");
+//	}
+//	else if (age<30)
+//	{
+//		printf("qingnian\n");
+//	}
+//	else if (age<50)
+//	{
+//		printf("zhongnian\n");
+//	}
+//	else
+//	{
+//		printf("laonian\n");
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//		int age = 0;
+//		scanf("%d", &age);
+//	
+//		if (age<18)
+//		{
+//			printf("shaonian\n");
+//		}
+//		else if (age >=18 && age<30)
+//		{
+//			printf("qingnian\n");
+//		}
+//		else if (age>=30 && age<50)
+//		{
+//			printf("zhongnian\n");
+//		}
+//		else
+//		{
+//			printf("laonian\n");
+//		}
+//		return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i%2==1)
+//		{
+//			printf("%d\n", i);
+//		}		
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int xingqi = 0;
+//	scanf("%d", &xingqi);
+//
+//	switch (xingqi)
+//	{
+//	case 1:
+//		printf("yue\n");
+//		break;
+//	case 2:
+//		printf("huo\n");
+//		break;
+//	case 3:
+//		printf("shui\n");
+//		break;
+//	case 4:
+//		printf("mu\n");
+//		break;
+//	case 5:
+//		printf("jin\n");
+//		break;
+//	case 6:
+//		printf("tu\n");
+//		break;
+//	case 7:
+//		printf("ri\n");
+//		break;
+//	default:
+//		printf("X\n");
+//		break;
+//	}
+//
+//	return 0;
+//}
