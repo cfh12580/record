f40237eda94059ed2716c6f341fcc7e25dbda4ff
蔡府宏 <cfh12580@gmail.com>
2021-10-04 22:22:05 +0900

    xunhuan1

---
+++ xunhuan.c
@@ -0,0 +1,125 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//
+//	while (i<=10)
+//	{
+//		if (i==4)
+//		{
+//			continue;
+//		}
+//		printf("%d\n", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//
+//	while (i <= 10)
+//	{
+//		if (i == 4)
+//		{
+//			break;
+//		}
+//		printf("%d\n", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//
+//	while (i <= 10)
+//	{
+//		printf("%d\n", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ch = 0;
+//	ch = getchar();
+//	putchar(ch);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch=getchar()) != EOF)
+//	{
+//		putchar(ch);
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char password[20] = { 0 };
+//	printf("shurumimaF");
+//	scanf("%s", password);
+//	int tmp = getchar();
+//	while ((tmp=getchar()) != '\n')
+//	{
+//		;
+//	}
+//
+//	printf("querenmima(Y/N)F");
+//	int ch = getchar();
+//	if (ch=='Y','y')
+//	{
+//		printf("chenggong");
+//	}
+//	else
+//	{
+//		printf("shibai");
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for ( i = 1; i <=5; i++)
+//	{
+//		printf("%d\n", i);
+//
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 5; i++)
+//	{
+//		if (i==3)
+//		{
+//			continue;
+//		}
+//		printf("%d\n", i);
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	for (i=0; i==0; i++)
+//			printf("%d\n", i);
+//
+//	return 0;
+//}
